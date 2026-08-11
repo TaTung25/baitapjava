5e019aa322c0f85095cb5643576d2623f70f8e04
TaTung25 <20231177.eaut.edu.vn>
2026-08-11 12:54:38 +0700

    update

---
+++ chuong2/src/main/java/org/tung/chuong2/Chuong2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.tung.chuong2;
+
+/**
+ *
+ * @author 56745654242453456656
+ */
+public class Chuong2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
