cfcf6f553b6a7e4004d9f1a30623c79417a5ced7
TaTung25 <20231177.eaut.edu.vn>
2026-08-20 09:38:52 +0700

    update

---
+++ formdn/src/main/java/org/tung/formdn/Formdn.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.tung.formdn;
+
+/**
+ *
+ * @author 56745654242453456656
+ */
+public class Formdn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
