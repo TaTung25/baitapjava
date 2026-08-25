0ef6d50d2d9f07f7f91806da12dd9f1256bea221
TaTung25 <20231177.eaut.edu.vn>
2026-08-25 09:30:26 +0700

    update

---
+++ chuong2/src/test/java/org/tung/chuong2/lab3bai8baitapmorongNGTest.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package org.tung.chuong2;
+
+import static org.testng.Assert.*;
+
+/**
+ *
+ * @author 56745654242453456656
+ */
+public class lab3bai8baitapmorongNGTest {
+    
+    public lab3bai8baitapmorongNGTest() {
+    }
+
+    @org.testng.annotations.BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.testng.annotations.AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.testng.annotations.BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @org.testng.annotations.AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Test of main method, of class lab3bai8baitapmorong.
+     */
+    @org.testng.annotations.Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        lab3bai8baitapmorong.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
